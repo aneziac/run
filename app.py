413b479ff7a1e5e404d267cd41886de76ef9e700
aneziac <nateannau@gmail.com>
2020-07-05 21:02:38 -0700

    add losing condition

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 import pygame as pg
 import math
-import os
 import random
 
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, threshold, polygon_verts):
+    def __init__(self, polygon_verts, world_map):
         self.sprite = pg.transform.smoothscale(pg.image.load(os.path.join('assets', 'sprite.png')), (100, 100))
         self.xpos = SCREEN_WIDTH // 2 - self.sprite.get_width() // 2
         self.iypos = SCREEN_HEIGHT - 110
 
         self.polygon_verts = polygon_verts
-        self.threshold = threshold * 0.8
+        self.world_map = world_map
+        self.threshold = SCREEN_HEIGHT // 2 * math.tan(math.pi / self.polygon_verts)
 
         self.counter = 0
 
@@ -19,11 +22,13 @@ class Player(pg.sprite.Sprite):
         self.yacc = -0.2
         self.yvel = 0
         self.rotation = 0
-        self.rotation_speed = 30
+        self.rotation_speed = 15
         super().__init__()
 
-    def update(self, keys):
-        if self.ypos < self.iypos:
+    def update(self, keys, depth):
+        face = round(((self.rotation / (2 * math.pi)) * -self.polygon_verts)) % self.polygon_verts
+
+        if self.ypos < self.iypos or self.world_map[round(depth) + 1][face] == 0 or self.ypos > self.iypos + 20:
             self.yvel += self.yacc
         elif keys[pg.K_w]:
             self.yvel = 8
@@ -31,17 +36,22 @@ class Player(pg.sprite.Sprite):
             self.yvel = 0
         self.ypos -= self.yvel
 
-        if keys[pg.K_a]:
-            self.xpos -= 5
-        if keys[pg.K_d]:
-            self.xpos += 5
+        if self.ypos > SCREEN_HEIGHT + 20:
+            print("You lost")
+            quit()
 
-        if self.xpos < SCREEN_WIDTH // 2 - self.threshold:
-            self.rotate_world(False)
-            self.sign = -1
-        elif self.xpos > SCREEN_WIDTH // 2 + self.threshold - self.sprite.get_width():
-            self.rotate_world()
-            self.sign = 1
+        if keys[pg.K_a]:
+            self.xpos -= 7
+        if keys[pg.K_d]:
+            self.xpos += 7
+
+        if self.counter == 0:
+            if self.xpos < SCREEN_WIDTH // 2 - self.threshold:
+                self.rotate_world(False)
+                self.sign = -1
+            elif self.xpos > SCREEN_WIDTH // 2 + self.threshold - self.sprite.get_width():
+                self.rotate_world()
+                self.sign = 1
 
         if self.counter < 0 and self.ypos >= self.iypos - 40:
             self.counter = self.rotation_speed
@@ -81,10 +91,12 @@ class World:
         self.world_map = [[1] * self.polygon_verts for _ in range(world_depth)]
         self.world_map = self.world_map + [[0] * self.polygon_verts for _ in range(2 * self.render_distance)]
 
-        for x in range(len(self.world_map)):
+        for x in range(len(self.world_map) - 10):
             for y in range(self.polygon_verts):
-                    if random.random() > 0.7:
-                        self.world_map[x][y] = 0         
+                    if random.random() > 0.6:
+                        self.world_map[x + 10][y] = 0
+
+        self.world_map[2][3] = 0       
 
     def project_vertices(self, depth):
         depth_offset = depth - math.floor(depth)
@@ -110,9 +122,10 @@ class Game:
         self.running = True
         self.screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
         self.depth = 1
+        self.game_speed = 0.05
 
-        self.world = World([0, 255, 0], 8)
-        self.player = Player(self.world.x_offset, self.world.polygon_verts)
+        self.world = World([0, 255, 0], 5)
+        self.player = Player(self.world.polygon_verts, self.world.world_map)
 
         self.game_loop()
 
@@ -127,9 +140,9 @@ class Game:
             projected_verts = self.world.project_vertices(self.depth)
             self.render_world(projected_verts)
 
-            self.player.update(keys)
+            self.player.update(keys, self.depth)
             self.render_player()
-            self.depth += 0.05
+            self.depth += self.game_speed
 
             pg.display.update()
 
@@ -142,7 +155,7 @@ class Game:
 
     def render_world(self, projected_verts):
         def build(x, v):
-            return [self.rotate(projected_verts[x], self.world.polygon_ang * v)]
+            return [self.rotate(projected_verts[x], self.world.polygon_ang * (v - 1))]
 
         # draw projected vertices
         for x in range(self.world.render_distance + self.world.back_render_distance - 1):
