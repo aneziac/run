e7505416b453c97e8f6e98b72267b7ecb6d07332
aneziac <nateannau@gmail.com>
2020-07-07 22:27:33 -0700

    add stars, anti aliasing

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 import pygame as pg
+from pygame import gfxdraw
 import math
 import random
 
@@ -14,14 +15,15 @@ class Player(pg.sprite.Sprite):
 
         self.polygon_verts = polygon_verts
         self.world_map = world_map
-        self.threshold = SCREEN_HEIGHT // 2 * math.tan(math.pi / self.polygon_verts)
 
+        self.threshold = SCREEN_HEIGHT // 2 * math.tan(math.pi / self.polygon_verts)
         self.counter = 0
+        self.rotation = 0
 
         self.ypos = self.iypos
         self.yacc = -0.2
         self.yvel = 0
-        self.rotation = 0
+
         self.rotation_speed = 15
         super().__init__()
 
@@ -80,12 +82,18 @@ class World:
 
         self.create_map(100)
         self.create_lines()
+        self.create_stars(80)
 
     def create_lines(self):
         self.vanish_length = ((SCREEN_HEIGHT / 2) / math.cos(math.pi / self.polygon_verts)) - self.vanish_polygon_radius
         self.line_scale = self.vanish_length / math.log(self.render_distance - self.back_render_distance, self.log_base)
         self.x_offset = round(SCREEN_WIDTH / 2 - ((SCREEN_HEIGHT / 2) * math.tan(math.pi / self.polygon_verts)))
         self.polygon_ang = 2 * math.pi / self.polygon_verts
+
+    def create_stars(self, num):
+        self.stars = []
+        for x in range(num):
+            self.stars.append([random.randint(0, SCREEN_WIDTH - 1), random.randint(0, SCREEN_HEIGHT - 1), random.randint(2, 4), random.randint(50, 255)])
 
     def create_map(self, world_depth):
         self.world_map = [[1] * self.polygon_verts for _ in range(world_depth)]
@@ -146,28 +154,35 @@ class Game:
 
             pg.display.update()
 
-    def rotate(self, coords, theta):
-        theta += self.player.rotation
-        x, y = coords
-        tx, ty = x - (SCREEN_WIDTH // 2), y - (SCREEN_HEIGHT // 2)
-        rx, ry = round(tx * math.cos(theta) - ty * math.sin(theta)), round(tx * math.sin(theta) + ty * math.cos(theta))
-        return [rx + (SCREEN_WIDTH // 2), ry + (SCREEN_HEIGHT // 2) - self.world.height_offset]
-
     def render_world(self, projected_verts):
         def build(x, v):
-            return [self.rotate(projected_verts[x], self.world.polygon_ang * (v - 1))]
+            return [rotate(projected_verts[x], self.world.polygon_ang * (v - 1))]
+
+        def rotate(coords, theta):
+            theta += self.player.rotation
+            x, y = coords
+            tx, ty = x - (SCREEN_WIDTH // 2), y - (SCREEN_HEIGHT // 2)
+            rx, ry = round(tx * math.cos(theta) - ty * math.sin(theta)), round(tx * math.sin(theta) + ty * math.cos(theta))
+            return [rx + (SCREEN_WIDTH // 2), ry + (SCREEN_HEIGHT // 2) - self.world.height_offset]
+
+        # draw stars
+        for x in self.world.stars:
+            pg.draw.circle(self.screen, [x[3]] * 3, rotate([x[0], x[1]], self.player.rotation), x[2])
 
         # draw projected vertices
         for x in range(self.world.render_distance + self.world.back_render_distance - 1):
             for v in range(self.world.polygon_verts):
                 if self.world.world_map[x + math.floor(self.depth)][v] == 1:
-                    pg.draw.polygon(self.screen, [0, 255 - x * 10, 0], build(x, v) + build(x + 1, v) + build(x + 1, v + 1) + build(x, v + 1))
+                    color = [0, 255 - x * 10, 0]
+                    vertices = build(x, v) + build(x + 1, v) + build(x + 1, v + 1) + build(x, v + 1)
+                    pg.draw.polygon(self.screen, color, vertices)
+                    pg.gfxdraw.aapolygon(self.screen, vertices, color)
 
+        # draw vanishing point
         pg.draw.circle(self.screen, (0, 0, 0), (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - self.world.height_offset), self.world.vanish_polygon_radius)
 
     def render_player(self):
         self.screen.blit(self.player.sprite, (round(self.player.xpos), round(self.player.ypos)))
-
 
 if __name__ == '__main__':
     SCREEN_WIDTH, SCREEN_HEIGHT = 900, 600
