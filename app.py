c4482716130ffba678b04adddc3da858b528fe96
aneziac <nateannau@gmail.com>
2020-07-08 22:37:34 -0700

    major refactoring

--- app.py
+++ app.py
@@ -4,37 +4,37 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame as pg
 from pygame import gfxdraw
 import math
-import random
+from random import random, randint
 
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, polygon_verts, world_map):
-        self.sprite = pg.transform.smoothscale(pg.image.load(os.path.join('assets', 'sprite.png')), (100, 100))
-        self.xpos = SCREEN_WIDTH // 2 - self.sprite.get_width() // 2
-        self.iypos = SCREEN_HEIGHT - 110
+    def __init__(self, world):
+        super().__init__()
+        self.SPRITE = pg.transform.smoothscale(pg.image.load(os.path.join('assets', 'sprite.png')), (100, 100))
 
-        self.polygon_verts = polygon_verts
-        self.world_map = world_map
+        self.world = world
 
-        threshold = SCREEN_HEIGHT // 2 * math.tan(math.pi / self.polygon_verts)
-        self.left_threshold = SCREEN_WIDTH // 2 - threshold - (self.sprite.get_width() // 2)
-        self.right_threshold = SCREEN_WIDTH // 2 + threshold - (self.sprite.get_width() // 2)
+        THRESHOLD = SCREEN_HEIGHT // 2 * math.tan(math.pi / self.world.POLYGON_VERTS)
+        self.LEFT_THRESHOLD = SCREEN_WIDTH // 2 - THRESHOLD - self.SPRITE.get_width() // 2
+        self.RIGHT_THRESHOLD = SCREEN_WIDTH // 2 + THRESHOLD - self.SPRITE.get_width() // 2
 
         self.counter = 0
         self.rotation = 0
+        self.ROTATION_SPEED = 15
 
-        self.ypos = self.iypos
-        self.yacc = -0.2
+        self.xpos = SCREEN_WIDTH // 2 - self.SPRITE.get_width() // 2
+        self.STRAFE_SPEED = 7
+
+        self.RESTING_YPOS = SCREEN_HEIGHT - 110
+        self.ypos = self.RESTING_YPOS
+        self.YACC = -0.2
         self.yvel = 0
 
-        self.rotation_speed = 15
-        super().__init__()
-
     def update(self, keys, depth):
-        face = round(((self.rotation / (2 * math.pi)) * -self.polygon_verts)) % self.polygon_verts
+        face = round(((self.rotation / (2 * math.pi)) * -self.world.POLYGON_VERTS)) % self.world.POLYGON_VERTS
 
-        if self.ypos < self.iypos or self.world_map[round(depth) + 1][face] == 0 or self.ypos > self.iypos + 20:
-            self.yvel += self.yacc
+        if self.ypos < self.RESTING_YPOS or self.world.world_map[round(depth) + 1][face] == 0 or self.ypos > self.RESTING_YPOS + 20:
+            self.yvel += self.YACC
         elif keys[pg.K_w]:
             self.yvel = 8
         else:
@@ -46,79 +46,76 @@ class Player(pg.sprite.Sprite):
             quit()
 
         if keys[pg.K_a]:
-            self.xpos -= 7
+            self.xpos -= self.STRAFE_SPEED
         if keys[pg.K_d]:
-            self.xpos += 7
+            self.xpos += self.STRAFE_SPEED
 
         if self.counter == 0:
-            if self.xpos < self.left_threshold:
-                self.counter = -self.rotation_speed
-                self.sign = -1
-            elif self.xpos > self.right_threshold:
-                self.counter = -self.rotation_speed
-                self.sign = 1
+            if self.xpos < self.LEFT_THRESHOLD or self.xpos > self.RIGHT_THRESHOLD:
+                self.counter = -1
+                self.direction = math.copysign(1, self.xpos - (SCREEN_WIDTH // 2))
 
-        if self.counter < 0 and self.ypos >= self.iypos - 40:
-            self.counter = self.rotation_speed
-            self.ypos = self.iypos - 100
-            self.xpos = SCREEN_WIDTH // 2 - (self.sprite.get_width() // 2)# - (self.sign * 50)
+        if self.counter < 0 and self.ypos >= self.RESTING_YPOS - 40:
+            self.counter = self.ROTATION_SPEED
+            self.ypos = self.RESTING_YPOS - 100
+            self.xpos = SCREEN_WIDTH // 2 - (self.SPRITE.get_width() // 2)
         elif self.counter > 0:
-            self.rotation += 2 * self.sign * math.pi / self.polygon_verts / self.rotation_speed
+            self.rotation += 2 * math.pi * self.direction / self.world.POLYGON_VERTS / self.ROTATION_SPEED
             self.counter -= 1
 
 
 class World:
     def __init__(self, color, verts):
-        self.world_color = color
-        self.polygon_verts = verts
+        self.WORLD_COLOR = color
+        self.POLYGON_VERTS = verts
 
-        self.log_base = 2
-        self.vanish_polygon_radius = 10
+        self.LOG_BASE = 2
+        self.VANISH_RADIUS = 10
 
-        self.render_distance = 15
-        self.back_render_distance = 2
+        self.RENDER_DISTANCE = 15
+        self.BACK_RENDER_DISTANCE = 2
 
         self.height_offset = 100
-        self.safe_area = 10
 
-        self.create_map(100)
+        self.create_map()
         self.create_lines()
-        self.create_stars(80)
+        self.create_stars()
 
     def create_lines(self):
-        self.vanish_length = ((SCREEN_HEIGHT / 2) / math.cos(math.pi / self.polygon_verts)) - self.vanish_polygon_radius
-        self.line_scale = self.vanish_length / math.log(self.render_distance - self.back_render_distance, self.log_base)
-        self.x_offset = round(SCREEN_WIDTH / 2 - ((SCREEN_HEIGHT / 2) * math.tan(math.pi / self.polygon_verts)))
-        self.polygon_ang = 2 * math.pi / self.polygon_verts
+        self.VANISH_LENGTH = ((SCREEN_HEIGHT / 2) / math.cos(math.pi / self.POLYGON_VERTS)) - self.VANISH_RADIUS
+        self.LINE_SCALE = self.VANISH_LENGTH / math.log(self.RENDER_DISTANCE - self.BACK_RENDER_DISTANCE, self.LOG_BASE)
+        self.X_OFFSET = round(SCREEN_WIDTH / 2 - ((SCREEN_HEIGHT / 2) * math.tan(math.pi / self.POLYGON_VERTS)))
+        self.CENTRAL_ANGLE = 2 * math.pi / self.POLYGON_VERTS
 
-    def create_stars(self, num):
+    def create_stars(self, amount=80):
         self.stars = []
-        for x in range(num):
-            self.stars.append([random.randint(0, SCREEN_WIDTH - 1), random.randint(0, SCREEN_HEIGHT - 1), random.randint(2, 4), random.randint(50, 255)])
+        size = max(SCREEN_WIDTH, SCREEN_HEIGHT)
+        for x in range(amount):
+            self.stars.append([randint(0, SCREEN_WIDTH - 1), randint(0, SCREEN_HEIGHT - 1), randint(2, 4), randint(50, 255)])
 
-    def create_map(self, world_depth):
-        self.world_map = [[1] * self.polygon_verts for _ in range(world_depth)]
-        self.world_map = self.world_map + [[0] * self.polygon_verts for _ in range(2 * self.render_distance)]
+    def create_map(self, safe_area=10, world_depth=100):
+        self.world_map = [[1] * self.POLYGON_VERTS for _ in range(world_depth)]
+        self.world_map = self.world_map + [[0] * self.POLYGON_VERTS for _ in range(2 * self.RENDER_DISTANCE)]
 
-        for x in range(len(self.world_map) - self.safe_area):
-            for y in range(self.polygon_verts):
-                if random.random() > 0.6:
-                    self.world_map[x + self.safe_area][y] = 0
+        for x in range(len(self.world_map) - safe_area):
+            for y in range(self.POLYGON_VERTS):
+                if random() > 0.6:
+                    self.world_map[x + safe_area][y] = 0
 
     def project_vertices(self, depth):
         depth_offset = depth - math.floor(depth)
         projected_verts = []
 
         # calculate location of projected vertices
-        for x in range(self.render_distance + self.back_render_distance):
-            if x < self.back_render_distance:
-                proj_dist = self.log_base ** (x + depth_offset + 1) * -self.line_scale
+        for x in range(self.RENDER_DISTANCE + self.BACK_RENDER_DISTANCE):
+            if x < self.BACK_RENDER_DISTANCE:
+                proj_dist = self.LOG_BASE ** (x + depth_offset + 1) * -self.LINE_SCALE
             else:
-                proj_dist = math.log(x - depth_offset + 1 - self.back_render_distance, self.log_base) * self.line_scale
+                proj_dist = math.log(x - depth_offset + 1 - self.BACK_RENDER_DISTANCE, self.LOG_BASE) * self.LINE_SCALE
 
-            projected_verts.append([round(proj_dist * math.sin(math.pi / self.polygon_verts)) + self.x_offset, round(SCREEN_HEIGHT - (proj_dist * math.cos(math.pi / self.polygon_verts)))])
+            projected_verts.append([round(proj_dist * math.sin(math.pi / self.POLYGON_VERTS)) + self.X_OFFSET, round(SCREEN_HEIGHT - (proj_dist * math.cos(math.pi / self.POLYGON_VERTS)))])
 
-        projected_verts[:self.back_render_distance - 1] = projected_verts[self.back_render_distance - 1:0:-1]
+        projected_verts[:self.BACK_RENDER_DISTANCE - 1] = projected_verts[self.BACK_RENDER_DISTANCE - 1:0:-1]
 
         return projected_verts
 
@@ -126,20 +123,25 @@ class World:
 class Game:
     def __init__(self):
         pg.init()
+        pg.event.set_blocked(None)
+        pg.event.set_allowed([pg.QUIT])
+        self.game_clock = pg.time.Clock()
+
         self.running = True
-        self.screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        self.screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT], pg.DOUBLEBUF)
+        self.screen.set_alpha(None)
         self.depth = 1
-        self.game_speed = 0.05
+        self.GAME_SPEED = 0.05
 
         self.world = World([0, 255, 0], 8)
-        self.player = Player(self.world.polygon_verts, self.world.world_map)
+        self.player = Player(self.world)
 
         self.game_loop()
 
     def game_loop(self):
         while self.running:
             keys = pg.key.get_pressed()
-            if pg.QUIT in [event.type for event in pg.event.get()] or keys[pg.K_ESCAPE]:
+            if len(pg.event.get()) > 0 or keys[pg.K_ESCAPE]:
                 self.running = False
 
             self.screen.fill([0] * 3)
@@ -149,13 +151,14 @@ class Game:
 
             self.player.update(keys, self.depth)
             self.render_player()
-            self.depth += self.game_speed
+            self.depth += self.GAME_SPEED
 
+            self.game_clock.tick()
             pg.display.update()
 
     def render_world(self, projected_verts):
         def build(x, v):
-            return [rotate(projected_verts[x], self.world.polygon_ang * (v - 1))]
+            return [rotate(projected_verts[x], self.world.CENTRAL_ANGLE * (v - 1))]
 
         def rotate(coords, theta):
             theta += self.player.rotation
@@ -169,26 +172,33 @@ class Game:
             self.draw_circle(rotate(x[:2], self.player.rotation), x[2], [x[3]] * 3)
 
         # draw projected vertices
-        for x in range(self.world.render_distance + self.world.back_render_distance - 1):
-            for v in range(self.world.polygon_verts):
+        for x in range(self.world.RENDER_DISTANCE + self.world.BACK_RENDER_DISTANCE - 1):
+            for v in range(self.world.POLYGON_VERTS):
                 if self.world.world_map[x + math.floor(self.depth)][v] == 1:
                     color = [0, 255 - x * 15, 0]
                     vertices = build(x, v) + build(x + 1, v) + build(x + 1, v + 1) + build(x, v + 1)
                     self.draw_polygon(vertices, color)
 
         # draw vanishing point
-        pg.draw.circle(self.screen, (0, 0, 0), (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - self.world.height_offset), self.world.vanish_polygon_radius)
+        pg.draw.circle(self.screen, (0, 0, 0), (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - self.world.height_offset), self.world.VANISH_RADIUS)
 
     def render_player(self):
-        self.screen.blit(self.player.sprite, (round(self.player.xpos), round(self.player.ypos)))
+        self.screen.blit(self.player.SPRITE, (round(self.player.xpos), round(self.player.ypos)))
 
     def draw_polygon(self, vertices, color):
         pg.gfxdraw.aapolygon(self.screen, vertices, color)
         pg.gfxdraw.filled_polygon(self.screen, vertices, color)
 
     def draw_circle(self, coords, radius, color):
-        pg.gfxdraw.aacircle(self.screen, coords[0], coords[1], radius, color)
-        pg.gfxdraw.filled_circle(self.screen, coords[0], coords[1], radius, color)
+        if coords[0] + radius > 0 and coords[0] - radius < SCREEN_WIDTH and coords[1] + radius > 0 and coords[1] - radius < SCREEN_HEIGHT:
+            pg.gfxdraw.aacircle(self.screen, coords[0], coords[1], radius, color)
+            pg.gfxdraw.filled_circle(self.screen, coords[0], coords[1], radius, color)
+
+    def is_onscreen(self, x, y=None):
+        if y is None:
+            x, y = x
+        return x > 0 and x < SCREEN_WIDTH and y > 0 and y < SCREEN_HEIGHT
+
 
 if __name__ == '__main__':
     SCREEN_WIDTH, SCREEN_HEIGHT = 900, 600
